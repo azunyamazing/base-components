687a354a2ee5ad94e5de6f50088e5462a112356c
Eriri <huangyangyang@shoplazza.com>
2023-02-13 17:15:45 +0800

    feat: 处理 react 多次 createroot 警告

---
+++ vite-plugin-react-render.ts
@@ -0,0 +1,41 @@
+import type { PluginOption } from "vite";
+
+export const renderAppPlugin = (): PluginOption => {
+  return {
+    name: 'transform-file',
+    enforce: 'pre',
+    transform(src: string, id: string) {
+      if (id.includes('/examples/index') && src.includes('App')) {
+        const code = `
+          import { createRoot } from 'react-dom/client';
+          let app = undefined;
+          ${src}
+          if (!window.appLoaded) {
+            app = createRoot(document.getElementById('app'));
+            window.appLoaded = true;
+          }
+          app.render(<App />);
+        `;
+        return {
+          code
+        }
+      }
+
+      if (id.includes('/examples/') && src.includes('App')) {
+        const code = `
+          import { createRoot } from 'react-dom/client';
+          let component = undefined;
+          ${src}
+          if (!window.componentLoaded) {
+            component = createRoot(document.getElementById('component'));
+            window.componentLoaded = true;
+          }
+          component.render(<App />);
+        `;
+        return {
+          code,
+        }
+      }
+    },
+  }
+}
--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+import { virtualTreePlugin } from './vite-plugin-react-virtual';
+import { renderAppPlugin } from './vite-plugin-react-render';
+
 export default defineConfig({
   build: {
     emptyOutDir: true,
     outDir: 'dist',
   },
-  plugins: [react()],
+  plugins: [virtualTreePlugin(), renderAppPlugin(), react()],
 })
